a22d3a11c2865ccb201709486273d2b3738eab66
Anda Stefania Ghita <104030121+AndaGhita@users.noreply.github.com>
2023-02-03 14:44:08 +0100

    Update linked_list_python.py

--- linked_list_python.py
+++ linked_list_python.py
@@ -7,6 +7,10 @@ class Node:
 
     def __str__(self):
         """ Return a string representation of the data """
+        str = ""
+        str +=f"{self.data}"
+        return str
+
 
 
 class Storage:
@@ -18,15 +22,38 @@ class Storage:
 
     def push(self, data):
         """ Create a Node to hold the data, then put it at the head of the list. """
-        new_storage = Storage(data)
-        new_storage.next = self.head
-        self.head = new_storage
+        new_storage = Node(data)
         
+        if self.head is None:
+            self.head = new_storage
+        else:
+            new_storage.next = self.head
+            self.head = new_storage
+
     def pop(self):
         """ Remove the head Node and return its data. """
+
+        if self.head is None:
+            return None
+        else:
+            temp_storage =self.head
+            self.head = self.head.next
+            return temp_storage.data
 
     def peek(self):
         """ Return the data from the head Node, without removing it. """
 
+        if self.head is None:
+           return False
+        else:
+            return self.head.data
+
+
     def isempty(self):
         """ Return True if the list is empty, otherwise False """
+
+        if self.head is None:
+            return True
+        else:
+            return False
+
